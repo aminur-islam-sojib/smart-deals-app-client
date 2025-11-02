36302e03ca2c8452156e45a8f5e2ac6ee92d6b4b
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-11-02 21:03:13 +0600

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import Layout from "./Layout/Layout.jsx";
+import Home from "./Pages/Home.jsx";
+import Registration from "./Pages/Registration.jsx";
+import Login from "./Pages/Login.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/register",
+        element: <Registration />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
