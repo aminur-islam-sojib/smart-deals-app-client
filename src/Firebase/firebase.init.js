ec3771aba2e36a25420fac5473d676e57791e895
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-11-06 11:50:19 +0600

    update env file

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwhHrlxceEhSDudW46GA8WjOOAgxdo5mc",
-  authDomain: "b-social-8eb0a.firebaseapp.com",
-  projectId: "b-social-8eb0a",
-  storageBucket: "b-social-8eb0a.firebasestorage.app",
-  messagingSenderId: "932776610188",
-  appId: "1:932776610188:web:3c61f660b435bf38472ea9",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
